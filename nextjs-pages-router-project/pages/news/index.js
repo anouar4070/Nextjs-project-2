34710dd93e832c7168bf7118fc9cefb87d12b917
anouar garbaa <g.anouar@yahoo.com>
2024-11-19 15:57:37 +0100

    Linking between pages

--- nextjs-pages-router-project/pages/news/index.js
+++ nextjs-pages-router-project/pages/news/index.js
@@ -1,6 +1,19 @@
 // http://localhost:3000/news
 
+import { Fragment } from "react";
+import Link from "next/link";
+
 function NewsPage() {
-  return <h1>The News Page</h1>;
+  return (
+    <Fragment>
+      <h1>The News Page</h1>
+      <ul>
+        <li>
+          <Link href="/news/nextjs">NextJS</Link>
+        </li>
+        <li>something else</li>
+      </ul>
+    </Fragment>
+  );
 }
 export default NewsPage;
