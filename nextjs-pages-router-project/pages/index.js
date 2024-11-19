d8b6f440ea920edcd8690ad502bf941752f5561e
anouar garbaa <g.anouar@yahoo.com>
2024-11-19 14:50:29 +0100

    getting started with NextJS Project (Page Router approach)

---
+++ nextjs-pages-router-project/pages/index.js
@@ -0,0 +1,7 @@
+// http://localhost:3000
+
+
+function HomePage() {
+  return <h1>The Home Page</h1>;
+}
+export default HomePage;
---
+++ nextjs-pages-router-project/pages/news.js
@@ -0,0 +1,6 @@
+// http://localhost:3000/news
+
+function NewsPage() {
+  return <h1>The News Page</h1>;
+}
+export default NewsPage;
